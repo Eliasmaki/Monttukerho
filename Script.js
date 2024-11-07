6c0a4b9d5f13eafd6954d307dcacc446a4a80855
Eliasmaki <eliasjuhani.maki@gmail.com>
2024-11-08 00:14:06 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,11 @@
+
+function valikkonappi(){
+    if (valikko.style.display === "none"){
+        alert("valikko painettu");
+        valikko.style.display = "block";
+    }
+    else{
+        valikko.style.display = "none";
+        alert("valikko painettu");
+    }
+}
